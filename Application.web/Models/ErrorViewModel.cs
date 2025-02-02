d0b0b529f0ad94a759523b56686e21666b5c489a
gelareh-gowhary <gelarehgowhary@gmail.com>
2025-02-02 20:04:08 +0330

    initial Project MVC

---
+++ Application.web/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Application.web.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
